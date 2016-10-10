9bb3cba4e123e51699e8679f72e6f2fde0689206
Hauxii <haukur91@gmail.com>
2016-10-10 14:06:05 +0000

    i dont even moar

---
+++ core/src/com/ru/tgra/berglindoma13_Haukura14/Cell.java
@@ -0,0 +1,15 @@
+package com.ru.tgra.berglindoma13_Haukura14;
+
+/**
+ * Created by Lenny on 10.10.2016.
+ */
+public class Cell {
+
+    public boolean southwall;
+    public boolean westwall;
+
+    Cell(boolean southwall, boolean westwall){
+        this.southwall = southwall;
+        this.westwall = westwall;
+    }
+}
